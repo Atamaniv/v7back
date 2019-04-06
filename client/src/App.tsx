b2426692846f979c40895c5059945a14cac78c68
atamaniv <atamaniv@gmail.com>
2019-04-06 22:44:23 +0300

    fetch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+interface State {
+  passArray:Array<string>
+}
+
+interface Props {
+  
+}
+
+class App extends Component < Props , State > {
+  constructor(props: Props) {
+    super(props);
+    this.state = { passArray :[]}
+    this.state.passArray.push("xxx")
+  }
+
+  componentDidMount=()=>{
+    //fetch('https://v7back.herokuapp.com/api/passwords')
+    fetch('/api/passwords')
+      .then(response => response.json())
+      .then(json => this.setState({passArray: json.Data } ))
+      .then(_ => console.log(this.state.passArray))      
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.tsx</code> and save to reload.
+            {
+              this.state.passArray[1]
+            }
           </p>
           <a
             className="App-link"
