784fa52742af14ac9365c8c6d4e0ac2fcd7f171b
Atamaniv <38530500+Atamaniv@users.noreply.github.com>
2019-10-10 15:43:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,23 +4,8 @@ const generatePassword = require('password-generator');
 var cors = require('cors');
 const app = express();
 
-var pgp = require("pg-promise")(/*options*/);
-//'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
-var db = pgp("postgres://vbkghqxpadgjvl:4d355ea3ea1cbd387f0669c18290a79edbc57ec3fee8b3735c0d2201d9206aa2@ec2-54-221-236-144.compute-1.amazonaws.com:5432/d16dm58hklisc8");
-
-app.use(cors());
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
-app.get('/api/create_table_users', (req,res)=> {
-  db.none('CREATE DATABASE $1:users', 'd16dm58hklisc8')
-    .then(data => {
-        console.log('successfully created');
-    })
-    .catch(error => {
-        console.log(error);
-    });  
-});
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
